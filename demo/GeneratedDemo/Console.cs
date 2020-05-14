f7bb82a472fec69f41cd5d098afc6570add34092
Jakub Šturc <jakub.sturc@gmail.com>
2020-05-14 23:09:10 +0200

    Remove dead code

--- demo/GeneratedDemo/Console.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace GeneratedDemo
-//{
-//    internal static class Console
-//    {
-//        private static int _counter = 0;
-
-//        public static void WriteLine(string line) => System.Console.WriteLine($"{_counter++:00}:{line}");
-//    }
-//}
